ba654db53a72ce354f99f9affb0fc12295b4299a
Amritesh Srivastava <amritesh.srivastava@booking.com>
2020-03-27 14:10:02 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# be-express
+### steps to run 
+PORT=3001 node index.js 
---
+++ config/index.js
@@ -0,0 +1,7 @@
+// Export environment variables
+module.exports = {
+    AWS_ACCESS_KEY_ID: '',// add key id
+    AWS_SECRET_ACCESS_KEY: '', // add key
+    S3_BUCKET: 'xray-corona-uploaded-images',
+    AWS_REGION: 'us-east-2'
+}
