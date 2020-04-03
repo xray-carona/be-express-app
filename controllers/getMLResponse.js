15247f86c9d4fbc43c5cef5ccedfb4c716c5b6dd
Amritesh Srivastava <amritesh.srivastava@booking.com>
2020-04-03 11:24:17 +0530

    parse patientInfo

--- controllers/getMLResponse.js
+++ controllers/getMLResponse.js
@@ -1,9 +1,19 @@
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); 
+
 const getMLResponseFromAPI = (req,res) => {
   
   // call API that runs ML model
   // return response as string 
   // req.url should have S3 url
-  res.json("Patient suffering from COVID-19");
+  console.log(req.body);
+  console.log('test-logs');
+  console.log(req.body.url);
+  res.json({ 
+  	annotated_img_url: 'https://xray-corona.s3.ap-south-1.amazonaws.com/1_annotated.png',
+  	covid_diagnosis: req.patientInfo.name 
+  });
 };
 
 exports.getMLResponse = ( req, res, next ) => {
