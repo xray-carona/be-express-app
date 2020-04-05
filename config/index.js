028aee5b49035282b884c09becbbadeddee9e358
Ronald <ronald1das@gmail.com>
2020-04-05 13:48:01 +0530

    added env variables for node-env and ml base url

--- config/index.js
+++ config/index.js
@@ -3,5 +3,7 @@ module.exports = {
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
     S3_BUCKET: process.env.S3_BUCKET,
-    AWS_REGION: process.env.AWS_REGION
+    AWS_REGION: process.env.AWS_REGION,
+    NODE_ENV:process.env.NODE_ENV,
+    ML_BASE_URL:process.env.ML_BASE_URL,
 };
